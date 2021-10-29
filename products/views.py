9a975a8157f7a6d852f5290c3ef75c20035f84c8
kerleada7 <kerleada7@mail.ru>
2021-10-29 17:27:51 +0300

    d2_lesson_1

--- products/views.py
+++ products/views.py
@@ -11,16 +11,25 @@ def index(request):
     return render(request, 'products/index.html', content)
 
 
-def products(request, id_category=None):
+def products(request, id_category=None, page_id=1):
     if id_category is None:
         list_products = Product.objects.all()
     else:
         list_products = Product.objects.filter(category=id_category)
 
+    paginator: Paginator = Paginator(list_products, per_page=3)
+    try:
+        p_list_products = paginator.page(page_id)
+    except PageNotAnInteger:
+        p_list_products = paginator.page(1)
+    except EmptyPage:
+        p_list_products = paginator.page(paginator.num_pages)
+
+
     links_menu_categories = ProductCategory.objects.all()
 
     content = {'title': 'GeekShop - Каталог',
-               'list_products': list_products,
+               'list_products': p_list_products,
                'links_menu_categories': links_menu_categories
                }
     return render(request, 'products/products.html', content)
